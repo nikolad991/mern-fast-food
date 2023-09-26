c4def150d1b59e3b8ba60b6817dda371af075923
Nikola <nikolad991@gmail.com>
2023-09-26 23:33:35 +0200

    Mobile nav

---
+++ frontend/src/components/MobileMenu.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { MdClose } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/food-logo-transparent.png";
+import { AiFillFacebook, AiFillInstagram, AiFillTwitterSquare } from "react-icons/ai";
+const MobileMenu = ({ setShowMobileMenu, cart }) => {
+  const handleClose = () => {
+    setShowMobileMenu(false);
+  };
+  return (
+    <nav className="absolute top-0 left-0 bg-red-400 min-h-screen w-full overscroll-none z-10">
+      <div className="absolute top-11 right-7 text-5xl animate-pulse ">
+        <MdClose onClick={() => setShowMobileMenu(false)} />
+      </div>
+
+      <div className="h-screen flex flex-col gap-8 items-center justify-center  text-white uppercase text-4xl font-yeseva">
+        <div className="bg-red-300 rounded-full w-32 h-32">
+          <img src={Logo} alt="" />
+        </div>
+
+        <NavLink onClick={handleClose} to="/">
+          Home
+        </NavLink>
+        <NavLink onClick={handleClose} to="/menu">
+          Menu
+        </NavLink>
+        <NavLink onClick={handleClose} to="/orders">
+          Orders
+        </NavLink>
+        <NavLink onClick={handleClose} to="/cart">
+          Cart ({cart.length})
+        </NavLink>
+        <NavLink onClick={handleClose} to="/contact">
+          Contact
+        </NavLink>
+        <div className="flex gap-4 items-center ">
+            <a href="https://instagram.com" target="_blank">
+              <AiFillInstagram size={40} />
+            </a>
+            <a href="https://facebook.com" target="_blank">
+              <AiFillFacebook size={40} />
+            </a>
+            <a href="https://twitter.com" target="_blank">
+              <AiFillTwitterSquare size={40} />
+            </a>
+          </div>
+      </div>
+    </nav>
+  );
+};
+
+export default MobileMenu;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import {
@@ -17,85 +17,90 @@ import {
 } from "react-icons/ai";
 
 import Logo from "../assets/food-logo-transparent.png";
+import MobileMenu from "./MobileMenu";
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   const cart = useSelector((state) => state.cart);
   const { userData } = useSelector((state) => state.auth);
   const linkHover = "hover:text-red-600 transition-all duration-600";
   return (
-    <nav className=" text-neutral-800 flex flex-col h-36 justify-center ">
-      <div className="w-full px-10 py-1 text-white bg-neutral-800 justify-between hidden md:flex">
-        <div className="flex gap-2 items-center hover:[&>a]:text-red-500 [&>a]:transition [&>a]:duration-500 ">
-          <a href="https://instagram.com" target="_blank">
-            <AiFillInstagram size={20} />
-          </a>
-          <a href="https://facebook.com" target="_blank">
-            <AiFillFacebook size={20} />
-          </a>
-          <a href="https://twitter.com" target="_blank">
-            <AiFillTwitterSquare size={20} />
-          </a>
+    <>
+      <nav className=" text-neutral-800 flex flex-col h-36 justify-center ">
+        <div className="w-full px-10 py-1 text-white bg-neutral-800 justify-between hidden md:flex">
+          <div className="flex gap-2 items-center hover:[&>a]:text-red-500 [&>a]:transition [&>a]:duration-500 ">
+            <a href="https://instagram.com" target="_blank">
+              <AiFillInstagram size={20} />
+            </a>
+            <a href="https://facebook.com" target="_blank">
+              <AiFillFacebook size={20} />
+            </a>
+            <a href="https://twitter.com" target="_blank">
+              <AiFillTwitterSquare size={20} />
+            </a>
+          </div>
+          <div className="flex items-center gap-6 hover:[&>a]:text-red-500 [&>a]:transition [&>a]:duration-500 ">
+            <a href="tel:012345678" className="flex gap-1 items-center">
+              <AiFillPhone />
+              (00) 000 111 222
+            </a>
+            <a href="email:info@food.xyz" className="flex gap-1 items-center">
+              <AiOutlineMail /> info@food.xyz
+            </a>
+          </div>
         </div>
-        <div className="flex items-center gap-6 hover:[&>a]:text-red-500 [&>a]:transition [&>a]:duration-500 ">
-          <a href="tel:012345678" className="flex gap-1 items-center">
-            <AiFillPhone />
-            (00) 000 111 222
-          </a>
-          <a href="email:info@food.xyz" className="flex gap-1 items-center">
-            <AiOutlineMail /> info@food.xyz
-          </a>
-        </div>
-      </div>
-      <div className="px-6 flex items-center justify-between">
-        <div className="flex h-full cursor-pointer ">
-          <Link to="/">
-            <img src={Logo} className="w-28 h-28" alt="logo" />
-          </Link>
-        </div>
-        <div className="hidden md:flex gap-12 text-2xl ">
-          <NavLink to="/" className={linkHover} href="">
-            Home
-          </NavLink>
-          <NavLink to="/menu" className={linkHover} href="">
-            Menu
-          </NavLink>
-          <NavLink to="/orders" className={linkHover} href="">
-            Orders
-          </NavLink>
-          <NavLink to="/contact" className={linkHover} href="">
-            Contact
-          </NavLink>
-        </div>
-        <div className="flex h-7 gap-5">
-          <MagnifyingGlassIcon className={`${linkHover} cursor-pointer`} />
-          <Link to="/cart" className="flex relative">
-            <ShoppingCartIcon className={`${linkHover} cursor-pointer`} />
-            <span className="absolute top-0 right-0 text-xs font-bold w-4 h-4 p-2 bg-red-300 rounded-full flex items-center justify-center">
-              {cart.length}
-            </span>
-          </Link>
-          {userData ? (
-            <Link
-              to="/profile"
-              className={`flex gap-1 ${linkHover} cursor-pointer`}
-            >
-              <div className="flex">
-                <UserIcon />
-              </div>
-              <div className="flex items-end">{userData?.firstName}</div>
+        <div className="px-6 flex items-center justify-between">
+          <div className="flex h-full cursor-pointer ">
+            <Link to="/">
+              <img src={Logo} className="w-28 h-28" alt="logo" />
             </Link>
-          ) : (
-            <Link to="/login" className="flex">
-              <ArrowRightOnRectangleIcon
-                className={`${linkHover} cursor-pointer`}
-              />
+          </div>
+          <div className="hidden md:flex gap-12 text-2xl ">
+            <NavLink to="/" className={linkHover} href="">
+              Home
+            </NavLink>
+            <NavLink to="/menu" className={linkHover} href="">
+              Menu
+            </NavLink>
+            <NavLink to="/orders" className={linkHover} href="">
+              Orders
+            </NavLink>
+            <NavLink to="/contact" className={linkHover} href="">
+              Contact
+            </NavLink>
+          </div>
+          <div className="flex h-7 gap-5">
+            <MagnifyingGlassIcon className={`${linkHover} cursor-pointer`} />
+            <Link to="/cart" className="flex relative">
+              <ShoppingCartIcon className={`${linkHover} cursor-pointer`} />
+              <span className="absolute top-0 right-0 text-xs font-bold w-4 h-4 p-2 bg-red-300 rounded-full flex items-center justify-center">
+                {cart.length}
+              </span>
             </Link>
-          )}
+            {userData ? (
+              <Link
+                to="/profile"
+                className={`flex gap-1 ${linkHover} cursor-pointer`}
+              >
+                <div className="flex">
+                  <UserIcon />
+                </div>
+                <div className="flex items-end">{userData?.firstName}</div>
+              </Link>
+            ) : (
+              <Link to="/login" className="flex">
+                <ArrowRightOnRectangleIcon
+                  className={`${linkHover} cursor-pointer`}
+                />
+              </Link>
+            )}
+          </div>
+          <div className="border border-white h-12 w-12 md:hidden">
+            <Bars4Icon onClick={() => setShowMobileMenu(true)} />
+          </div>
         </div>
-        <div className="border border-white h-12 w-12 md:hidden">
-          <Bars4Icon />
-        </div>
-      </div>
-    </nav>
+      </nav>
+      {showMobileMenu && <MobileMenu setShowMobileMenu={setShowMobileMenu} cart={cart} />}
+    </>
   );
 };
 
